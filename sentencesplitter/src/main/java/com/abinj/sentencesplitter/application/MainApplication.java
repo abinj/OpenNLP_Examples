65ae4b9d5915c6f43077aa4ed7e097743580b6ff
abinj <abinmj656@gmail.com>
2017-09-03 18:14:30 +0530

    Sentence splitter comleted with the help of opennlp trained model.

--- sentencesplitter/src/main/java/com/abinj/sentencesplitter/application/MainApplication.java
+++ sentencesplitter/src/main/java/com/abinj/sentencesplitter/application/MainApplication.java
@@ -1,4 +1,28 @@
 package com.abinj.sentencesplitter.application;
 
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.InvalidFormatException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainApplication {
+
+    public static void main(String[] args) throws InvalidFormatException, IOException {
+        String paragraph = "Hi. How are you? This is Mike.";
+
+        //Always start with a model, a model is learned from training data
+        //Give proper path to the "en-sent.bin" file
+        InputStream is = new FileInputStream("en-sent.bin");
+        SentenceModel model = new SentenceModel(is);
+        SentenceDetectorME sDetector = new SentenceDetectorME(model);
+        String[] sentences = sDetector.sentDetect(paragraph);
+
+        for (String s : sentences) {
+            System.out.println(s);
+        }
+        is.close();
+    }
 }
